61eab69ffbca0ecc92a813d8862f13c8c87de383
dayeonii <135932646+dayeonii@users.noreply.github.com>
2026-09-07 21:25:49 +0900

    [Passed] 친구의 키 (789ms, 32MB)

---
+++ trail6/친구의 키/height-of-friends.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); // node
+        int m = Integer.parseInt(st.nextToken()); // edge
+
+        // 인접리스트 초기화
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        int[] indegree = new int[n + 1];
+
+        // 간선 입력
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(b);
+            indegree[b]++;
+        }
+
+        // kahn's algorithm
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            result.add(current);
+
+            for (int next : graph.get(current)) {
+                indegree[next]--;
+
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+
+        if (result.size() != n) {
+            System.out.println("그래프에 사이클이 있습니다");
+        } else {
+            for (int node : result) {
+                System.out.print(node + " ");
+            }
+        }
+    }
+}
